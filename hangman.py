c8a0b746376ab56dcdcf37329f8e22a2cc50b65d
Atebisun David <davidolaoluwa240@gmail.com>
2025-01-22 15:55:17 +0100

    Done with the hangman game

--- hangman.py
+++ hangman.py
@@ -1,120 +1,154 @@
 # Challenge
-#
+# Create a word guessing game
 
 # Import modules
-from random import randint
+import os
+import platform
+import time
+from random import choice
 
+'''
+    Function to clear terminal
+'''
+def clear_terminal():
+    if platform.system() == "Windows": os.system("cls")
+    else: os.system("clear")
+
+'''
+    Function to get random hidden word
+'''
+def get_hidden_word():
+    # Get words from file
+    with open("hangman_words.txt", "r") as file:
+        return choice(file.read().split())
+
+'''
+    Function to print message to the screen
+'''
+def print_msg(msg):
+    # Pause for 1 seconds
+    time.sleep(2)
+
+    # Clear terminal
+    clear_terminal()
+
+    print("======================================")
+    print(msg)
+    print("======================================")
+
+
+
+'''
+    Function to handle user guess not in hidden guess
+'''
+def handle_user_guess_fail(user_guess, user_correct_guess_str, attempts_left):
+    print_msg(f"{user_correct_guess_str.upper()}\n\n{user_guess.upper()} do not appears in the word!\nYou have {attempts_left} lives remaining.")
+
+    match attempts_left:
+        case 0:
+            print("----------")
+            print("   ( )-|  ")
+            print("  - | -    ")
+            print(r"   / \     ")
+
+        case 1:
+            print("----------")
+            print("   ( )-   ")
+            print("  - | -    ")
+            print(r"   / \     ")
+
+        case 2:
+            print("----------")
+            print("   ( )    ")
+            print("  - | -    ")
+            print(r"   / \     ")
+
+        case 3:
+            print("----------")
+            print("   ( )    ")
+            print("  - | -    ")
+            print("   /       ")
+
+        case 4:
+            print("----------")
+            print("   ( )    ")
+            print("  - | -    ")
+            print("           ")
+
+        case 5:
+            print("----------")
+            print("   ( )    ")
+            print("    |      ")
+            print("           ")
+
+        case 6:
+            print("----------")
+            print("   ( )    ")
+            print("           ")
+            print("           ")
+
+'''
+    Function to handle user guess in hidden guess
+'''
+def handle_user_guess_correct(hidden_guess, user_guess, user_correct_guess):
+    for i in range(len(hidden_guess)):
+        guess_char = hidden_guess[i]
+        if guess_char == user_guess:
+            user_correct_guess[i] = user_guess
+
+'''
+    Function for the hangman game
+'''
 def hangman():
     # States
-    words = []
-    user_correct_guess = []
+    hidden_guess = get_hidden_word()
+    hidden_guess_length = len(hidden_guess)
+    user_correct_guess = ["_"] * hidden_guess_length
+    user_correct_guess_str = "".join(user_correct_guess)
     attempts_left = 7
 
-    # 1. Get words from file
-    with open("hangman_words.txt", "r") as file:
-        # 1a. Update words list
-        words.extend(file.read().split())
+    print_msg(f"Welcome Guest\nYou have {attempts_left} lives.")
 
-    # 2. Get hidden guess and length of hidden guess
-    hidden_guess = words[randint(0, len(words) - 1)]
-    hidden_guess_length = len(hidden_guess)
-    print("Your hidden word: ", hidden_guess)
+    while attempts_left != 0 and user_correct_guess_str != hidden_guess:
+        print_msg(f"Here's the word:\n\n{user_correct_guess_str.upper()} ({hidden_guess_length} letters)")
 
-    user_correct_guess = ["_"] * hidden_guess_length
+        # Get user guess
+        user_guess = input("\nEnter the character you think the word may have: ").lower()
 
-    # 3. While user still have attempts live and user correct guess is not equal to hidden guess keep playing game
-    while attempts_left != 0 and "".join(user_correct_guess) != hidden_guess:
-        # 3a. Print message
-        print(f"""Here's the word:
-{"".join(user_correct_guess)}({hidden_guess_length} letters)
-You have {attempts_left} lives remaining.
-""")
-
-        # 3b. Get user guess
-        user_guess = input("Enter the character you think the word may have: ").lower()
-
-        # 3c. validate user input
+        # Validate user input
         if len(user_guess) > 1:
-            print("===================\n===================")
-            print("Error💥💥: You can only enter a single letter")
-            print("===================\n===================")
+            print_msg("Error💥💥: You can only enter a single letter")
             continue
 
-        #
+        # Check if user_guess is not in hidden guess
         if user_guess not in hidden_guess:
+            # Decrement number of attempt
             attempts_left -= 1
 
-            print(f"""
-Good choice! Let's see if {user_guess.upper()} is in the word...
-{"".join(user_correct_guess)}
-{user_guess.upper()} do not appears in the word!
-You have {attempts_left} lives remaining.
-            """)
+            # Handle user guess fail
+            handle_user_guess_fail(user_guess, user_correct_guess_str, attempts_left)
 
-            match attempts_left:
-                case 0:
-                    print("----------")
-                    print("   ( )-|  ")
-                    print("  - | -    ")
-                    print(r"   / \     ")
-
-                case 1:
-                    print("----------")
-                    print("   ( )-   ")
-                    print("  - | -    ")
-                    print(r"   / \     ")
-
-                case 2:
-                    print("----------")
-                    print("   ( )    ")
-                    print("  - | -    ")
-                    print(r"   / \     ")
-
-                case 3:
-                    print("----------")
-                    print("   ( )    ")
-                    print("  - | -    ")
-                    print("   /       ")
-
-                case 4:
-                    print("----------")
-                    print("   ( )    ")
-                    print("  - | -    ")
-                    print("           ")
-
-                case 5:
-                    print("----------")
-                    print("   ( )    ")
-                    print("    |      ")
-                    print("           ")
-
-                case 6:
-                    print("----------")
-                    print("   ( )    ")
-                    print("           ")
-                    print("           ")
-
+        # Check if user_guess is in hidden guess and if user_guess already exist in the user_correct_guess
         if user_guess in hidden_guess and user_guess in user_correct_guess:
-                    print(f"""
-You've already guessed {user_guess.upper()}!
-{"".join(user_correct_guess)}
-You have {attempts_left} lives remaining.
-                    """)
+            print_msg(f"You've already guessed {user_guess.upper()}!\n\n{user_correct_guess_str.upper()}")
 
+        # Check if user_guess is in hidden guess and if user_guess does not exist in the user_correct_guess
         if user_guess in hidden_guess and user_guess not in user_correct_guess:
-            for i in range(len(hidden_guess)):
-                guess_char = hidden_guess[i]
-                if guess_char == user_guess:
-                    user_correct_guess[i] = user_guess
+            # Handle user guess correct
+            handle_user_guess_correct(hidden_guess, user_guess, user_correct_guess)
 
-            print(f"""
-Good choice! Let's see if {user_guess.upper()} is in the word...
-{"".join(user_correct_guess)}
-{user_guess.upper()} appears in the word!
-You have {attempts_left} lives remaining.
-            """)
-            print("===================\n===================")
+            # Update user_correct_guess_str
+            user_correct_guess_str = "".join(user_correct_guess)
 
+            print_msg(f"{user_correct_guess_str.upper()}\n\n{user_guess.upper()} appears in the word!")
+    else:
+        # Check if user guessed the hidden word
+        if user_correct_guess_str == hidden_guess:
+            print_msg(f"YOU WON 🔥🔥: ({hidden_guess})")
+
+        # Check if user could not guessed the hidden word
+        if attempts_left == 0:
+            print_msg(f"YOU LOSE 😢: ({hidden_guess}) is the hidden word")
 
 # Start Game
 hangman()
